d993108cdb9d0a51f115e5117ff0bb637fa6783c
CaelanRT <caelanrt15@gmail.com>
2025-02-06 21:35:51 -0500

    Auth flow complete, db storage works and can dynamically display user data in the dashboard page. Routing works as well

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,30 @@
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function Dashboard() {
     const navigate = useNavigate();
+    const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        const fetchUserData = async () => {
+            if (!auth.currentUser) return navigate("/login");
+
+            const userRef = doc(db, "users", auth.currentUser.uid);
+            const userSnap = await getDoc(userRef);
+
+            if (userSnap.exists()) {
+                setUser(userSnap.data());
+            } else {
+                console.log("No user data found in Firestore.");
+            }
+        };
+    
+        fetchUserData();
+    }, [navigate]);
+
+    //logout function
     const logout = async () => {
         await auth.signOut();
         navigate("/login");
@@ -11,9 +32,18 @@ export default function Dashboard() {
 
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Welcome! You are logged in.</p>
-            <button onClcik = {logout}>Logout</button>
+            {user ? (
+                <div>
+                    <img src={user.photoURL} alt="User" width="100" style={{ borderRadius: "50%"}} />
+                    <h2>{user.displayName}</h2>
+                    <p>Email: {user.email}</p>
+                    <button onClick={logout}>Logout</button>
+                </div>
+            ) : (
+                <p>Loading user data...</p>
+            )
+            
+        }
         </div>
     );
 }
